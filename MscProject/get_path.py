e39961095200a56ac28c36a5d2ac1bb00f4311c3
YuZehanghhh <78395552+YuZehanghhh@users.noreply.github.com>
2024-08-08 18:37:54 +0100

    Update get_path.py

--- MscProject/get_path.py
+++ MscProject/get_path.py
@@ -1,17 +1,17 @@
 import clip
 import torch
 
-# 加载预训练的 CLIP 模型
+# load CLIP
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-L/14", device=device)
 
 last_layer = model.visual.transformer.resblocks[-1].ln_2
 
-# 保存最后一层的状态字典
+# Save last layer
 torch.save(last_layer.state_dict(), 'last_layer.pth')
-# 加载保存的最后一层
+# Load last layer
 last_layer_weights = torch.load('last_layer.pth')
 
-# 打印权重和偏置的细节
+# 
 print("Weight tensor:", last_layer_weights['weight'].shape)  # 如果有权重
 print("Bias tensor:", last_layer_weights['bias'].shape)  # 如果有偏置
